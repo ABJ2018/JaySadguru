0cf3143aa82da753f202d276aefc8fedb03fc226
ABJ2018 <amitjadhav2380@gmail.com>
2018-12-04 16:04:19 +0530

    New Emp class is created by Amit

---
+++ src/com/Method_overloadindScnrois/Emp.java
@@ -0,0 +1,21 @@
+package com.Method_overloadindScnrois;
+
+public class Emp 
+{
+	void m1()
+	{
+		System.out.println("m1");
+	}
+	void m2()
+	{
+		System.out.println("m2");
+	}
+	
+	public static void main(String[] args)
+	{
+	  	Emp e=new Emp();
+	  	e.m1();
+	  	e.m2();
+	}
+
+}
